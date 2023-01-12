b44cbdac44984d2d15b794295e4c8350896fd88e
KUNQI WAN <56388431+kunqiwan@users.noreply.github.com>
2023-01-12 16:21:29 +0100

    Create lec1876.go

---
+++ lec1876.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "aababcabc"
+	fmt.Println(countGoodSubstrings(s))
+}
+
+func countGoodSubstrings(s string) int {
+left :=0
+right :=0
+count :=0
+for right < len(s){
+	right ++
+	if right -left ==3{
+		if s[right-1]!=s[right-2] &&s[right-1]!=s[right-3] && s[right-3]!=s[right-2]{
+			count++
+		}
+		left++
+	}
+}
+return count
+}
