9a579003a17df6390bce44bf55dd8f8b2b131825
KUNQI WAN <56388431+kunqiwan@users.noreply.github.com>
2023-02-19 00:35:01 +0100

    Create lec242.go

---
+++ lec242.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+
+}
+func isAnagram(s string, t string) bool {
+s1 :=occ(s)
+t1 :=occ(t)
+if s1 ==t1{
+	return true
+} else {
+	return false
+}
+}
+func occ(target string) [26]int{
+	save :=[26]int{}
+	for n:=0;n<len(target);n++{
+		curr :=target[n]
+		save[curr-'a']++
+	}
+	return  save
+}
