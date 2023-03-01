2fb9b427ab1139e28ef8c6fcc20ea5561c796316
KUNQI WAN <56388431+kunqiwan@users.noreply.github.com>
2023-03-01 23:54:33 +0100

    Create lec2529.go
    binary search - range

---
+++ lec2529.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+//For Loop Method
+
+//func maximumCount(nums []int) int {
+//	neg:=0
+//	pos:=0
+//for n:=0;n<len(nums);n++{
+//	if nums[n] <0{
+//		neg++
+//	}
+//	if nums[n]>0{
+//		pos++
+//	}
+//}
+//if neg>=pos{
+//	return neg
+//} else{
+//	return pos
+//}
+//}
+func main() {
+	nums :=[]int{-2,-1,-1,1,2,3}
+	fmt.Println(maximumCount(nums))
+
+}
+func maximumCount(nums []int) int {
+	nr:=neg(nums)
+	pr :=pos(nums)
+	if nr>=pr{
+		return nr
+	} else {
+		return  pr
+	}
+}
+func neg(nums []int) int {
+	length :=len(nums)
+	right := length-1
+	left :=0
+	//find negative range,right range
+	for left<=right{
+		mid :=left+(right-left)/2
+		if nums[mid]>0{
+			right = mid-1
+		} else if nums[mid] ==0{
+			right = mid -1
+		} else if nums[mid] <0{
+			left =mid+1
+		}
+	}
+	nr := right+1
+	return nr
+}
+func pos(nums []int)int{
+	length :=len(nums)
+	right := length-1
+	left :=0
+	//find positive range， left range
+	for left<=right{
+		mid :=left+(right-left)/2
+		if nums[mid]<=0{
+			left = mid+1
+
+		} else {
+			right = mid-1
+		}
+	}
+	pr:=length-left
+	return pr
+
+}
